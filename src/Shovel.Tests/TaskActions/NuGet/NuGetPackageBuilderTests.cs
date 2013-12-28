43dc23d505cdf647fe50317f78db46c9ffbb5a85
Janco Wolmarans <jancow@gmail.com>
2013-12-28 22:14:06 +0200

    Cover NuGet package builder with test

---
+++ src/Shovel.Tests/TaskActions/NuGet/NuGetPackageBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using ShovelPack.TaskActions.NuGet;
+
+namespace ShovelPack.Tests.TaskActions.NuGet
+{
+	[TestFixture]
+	public class NuGetPackageBuilderTests
+	{
+		[Test]
+		public void CanBuildNuGetPackage()
+		{
+			DeleteAllNuGetPackages();
+
+			var builder = new NuGetPackageBuilder(@"..\..\..\..\tools\NuGet\NuGet.exe");
+			builder.BuildNuGetPackage(new NuGetPackCmdConfigurator() { NuSpec = @"Acceptance\NuGet\test-nuget-pack.nuspec", OutputDirectory = "." });
+
+			Assert.IsTrue(GetPackageFileList().Any(), "No NuGet package files found");
+		}
+
+		private static void DeleteAllNuGetPackages()
+		{
+			GetPackageFileList()
+				.ForEach(File.Delete);
+		}
+
+		private static List<string> GetPackageFileList()
+		{
+			return
+				Directory.EnumerateFiles(".", "*.nupkg")
+				.ToList();
+		}
+	}
+}
--- src/Shovel/TaskActions/NuGet/NuGetPackageBuilder.cs
+++ src/Shovel/TaskActions/NuGet/NuGetPackageBuilder.cs
@@ -4,15 +4,21 @@ namespace ShovelPack.TaskActions.NuGet
 {
 	public class NuGetPackageBuilder
 	{
-		private const string NuGetExePath = "bin\\Nuget.exe";
+		private readonly string _nugetExePath;
+
+		public NuGetPackageBuilder(string nugetExePath = null)
+		{
+			_nugetExePath = nugetExePath ?? "bin\\Nuget.exe";
+		}
 
 		public void BuildNuGetPackage(INuGetPackConfiguration packConfiguration)
 		{
 			var processRunner = new ProcessRunner();
-			processRunner.RunProcess(new ProcessProperties()
+			processRunner.RunProcess(
+				new ProcessProperties()
 				{
-					Arguments = new[] {"pack", packConfiguration.NuSpec, "-OutputDirectory", packConfiguration.OutputDirectory},
-					Executable = NuGetExePath
+					Arguments = new[] { "pack", packConfiguration.NuSpec, "-OutputDirectory", packConfiguration.OutputDirectory },
+					Executable = _nugetExePath
 				});
 		}
 	}
