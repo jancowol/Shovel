f462d7742f009bad14c035c712e867abf2d3b367
Janco Wolmarans <jancow@gmail.com>
2013-12-28 23:59:14 +0200

    Completed acceptance test for NuGet packaging task

--- src/Shovel.Tests/Acceptance/NuGet/NuGetPackAcceptanceTests.cs
+++ src/Shovel.Tests/Acceptance/NuGet/NuGetPackAcceptanceTests.cs
@@ -1,21 +1,40 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
 
 namespace ShovelPack.Tests.Acceptance.NuGet
 {
 	[TestFixture]
 	public class NuGetPackAcceptanceTests : ShovelAcceptanceTestsBase
 	{
-		//[Test]
-		//public void GivenAManifestFile_WhenThePackTaskIsRun_ThenANugetPackageIsCreated()
-		//{
-		//	"Pack"
-		//		.Do()
-		//		.NuGet.Pack(n =>
-		//			{
-		//				n.NuSpec = @"Acceptance\NuGet\test-nuget-pack.nuspec";
-		//				n.OutputDirectory = @".";
-		//			})
-		//		.Run();
-		//}
+		[Test]
+		public void GivenAManifestFile_WhenThePackTaskIsRun_ThenANugetPackageIsCreated()
+		{
+			DeleteAllNuGetPackageFiles();
+
+			"Pack"
+				.Do()
+				.NuGet.Pack(n =>
+					{
+						n.NuSpec = @"Acceptance\NuGet\test-nuget-pack.nuspec";
+						n.OutputDirectory = @".";
+					})
+				.Run();
+
+			Assert.That(GetPackageFileList().Any(), "No NuGet package files found");
+		}
+
+		private static void DeleteAllNuGetPackageFiles()
+		{
+			GetPackageFileList()
+				.ToList()
+				.ForEach(File.Delete);
+		}
+
+		private static IEnumerable<string> GetPackageFileList()
+		{
+			return Directory.EnumerateFiles(".", "*.nupkg");
+		}
 	}
 }
